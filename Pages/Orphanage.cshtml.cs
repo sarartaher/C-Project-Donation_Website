33c489b4031b896d363bf89793fbf894271d8c0c
sarartaher <saifurtahersarar7@gmail.com>
2025-09-02 13:03:07 +0600

    Second commit

---
+++ Pages/Orphanage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Donation_Website.Pages
+{
+    public class OrphanageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
