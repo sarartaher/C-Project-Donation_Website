961a95e0f5f0ca9362a1d663315e377472c20708
sarartaher <saifurtahersarar7@gmail.com>
2025-09-11 12:49:34 +0600

    Added pictures

---
+++ Pages/ManageRoles.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Donation_Website.Pages
+{
+    public class ManageRolesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
