18eb545ca2deced2057226cd7cffa66dca73dcf5
sarartaher <saifurtahersarar7@gmail.com>
2025-09-14 09:58:46 +0600

    Some more Updates

---
+++ Models/FinanceLog.cs
@@ -0,0 +1,14 @@
+﻿namespace Donation_Website.Models
+{
+    public class FinanceLog
+    {
+        public int FinanceLogId { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public decimal ChangesinAmount { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public int DonorID { get; set; }
+        public int DonationId { get; set; }
+        public int ProjectId { get; set; }
+    }
+}
