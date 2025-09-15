e566d8eeef6be048264084f01159f859753ce04d
sarartaher <saifurtahersarar7@gmail.com>
2025-09-16 01:28:19 +0600

    new updates

---
+++ Pages/ResetPassword.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Donation_Website.Pages
+{
+    public class ResetPasswordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
